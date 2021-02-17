d0d823ab28ab01e486f25210f7e21bf91d472594
Aidana Khankel <aidanakhankeldieva91@gmail.com>
2021-02-16 21:02:23 -0600

    change font size about me page

--- src/components/About.js
+++ src/components/About.js
@@ -42,7 +42,7 @@ function About() {
             alt={author.name}
           />
           <div className='text-lg flex flex-col justify-center'>
-            <h1 className='cursive text-6xl text-green-300 mb-4'>
+            <h1 className='cursive text-3xl md:text-6xl text-green-300 mb-4'>
               Hey there. I'm{" "}
               <span className='text-green-100'>{author.name}</span>
             </h1>
